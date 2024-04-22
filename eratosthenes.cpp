12add6e58824fd3151f63383fb399ace8dd4a635
Булат <nevatorr@student.21-school.ru>
2024-04-23 00:46:53 +0300

    done2.1

--- eratosthenes.cpp
+++ eratosthenes.cpp
@@ -10,15 +10,11 @@ std::vector<int> sieve(int n) {
     }
     for (ptrdiff_t i = 2; i < n + 1; i++) {
         if (input[i] != 0) {
-            new_size++;
+            result.push_back(input[i]);
             for (ptrdiff_t j = i * i; j < n + 1; j += i) {
                 input[j] = 0;
             }
         }
-    }
-    result.resize(new_size);
-    for(ptrdiff_t i = 0; i < n + 1; i++){
-        if(input[i] != 0) result.push_back(input[i]);
     }
     return result;
 }
